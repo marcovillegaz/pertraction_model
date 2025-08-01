2cd178c81a9ec4a7479747253dc132047ad8356e
Marco Asus Laptop <marcovillegas.g@gmail.com>
2025-08-01 18:55:41 -0400

    compute fick's diffusivities

--- main.m
+++ main.m
@@ -5,9 +5,8 @@ polymerList = {"polystyrene"}
 compoundNames = {"benzene","methylAcetate","polystyrene"}  
 compoundDataFolder = "data/test-compounds";
 
-x = [0.1, 0.6, 0.3]    % test molar fraction
-T = 273.15
-n=10000
+molar_fraction = [0.1, 0.6, 0.3]    % test molar fraction in the membrane
+temperature = 273.15 % temperature of the system
 
 %% LOAD COMPOUND DATA
 % Load compound data and save in library
@@ -16,16 +15,5 @@ compoundLibrary = initCompoundLibrary(compoundNames,compoundDataFolder)
 unifacLibrary = loadUnifacData("unifac-test.xlsx")
 
 %% COMPUTE SOMETHING
-% Compute activity coefficient
-% LnGamma = activityCoefficients('unifac-vdw-fv', compoundLibrary,unifacLibrary, x, T, n)
-LnGamma = activityCoefficients('unifac-test', compoundLibrary,unifacLibrary, x, T, n)
-
-
-% % Save to no reload every time
-% save(fullfile(compoundDataFolder,"compoundLibrary.mat"),"compoundLibrary") 
-% 
-% load(fullfile(compoundDataFolder,"compoundLibrary.mat"))
-% % Compute mutual diffusion ciefficient matrix
-mutual_diffusion = mutualDiffusion(x,300,compoundLibrary)
-
-
+fickDiffusivity = computeFickDiffusivity(...
+    compoundLibrary,unifacLibrary, temperature, molar_fraction)
